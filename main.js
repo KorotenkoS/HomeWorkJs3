3e8890c4d9114c1edbd21d6138c207a6cfd3261a
KorotenkoS <sergey588@ua.fm>
2019-11-25 23:04:37 +0200

    click v JS i novie funkcii perebora

--- main.js
+++ main.js
@@ -4,16 +4,20 @@
 
  var colors = ['red', 'blue', 'white', 'yellow', 'green', 'black', 'orange', 'gray', 'brown', 'pink'];
 
- var btnNext = document.querySelector('.btnNext');
+ var btnNext = document.querySelector('#btnNext');
 
  var btnPrev = document.querySelector('#btnPrev');
 
- //  btnNext.onclick = renderNext();
+ btnNext.onclick = renderNext; // передаем ссылку на функцию
+ btnPrev.onclick = () => renderPrev(); // стрелочная функция 
 
  function renderNext() {
-     document.getElementById('kvadrat').style.cssText = `background-color: ${colors[i++ % colors.length]};`;
+     i < colors.length - 1 ? i++ : i = 0; // если(?) И меньше последнего индекса(длина массива минус 1), плюсуем 1. Иначе(:) И присвоить 0.
+     document.getElementById('kvadrat').style.cssText = `background-color: ${colors[i]};`;
  };
 
  function renderPrev() {
-     document.getElementById('kvadrat').style.cssText = `background-color: ${colors[i-- % colors.length]};`;
+     i > 0 ? i-- : i = colors.length - 1; // если(?) И больше 0б вычитаем 1. Иначе(:) И присвоить последний индекс массива(длина массива минус 1).
+     document.getElementById('kvadrat').style.cssText = `background-color: ${colors[i]};`;
+     console.log(i)
  };
